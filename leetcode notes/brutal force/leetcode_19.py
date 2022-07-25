89a4cb3e94625733ac017c738e4c355e04521971
rachelbian <109751487+rachelhappy@users.noreply.github.com>
2022-07-26 00:22:42 +0800

    Create leetcode_19.py

---
+++ leetcode notes/brutal force/leetcode_19.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        def getLength(head: ListNode) -> int:
+            length = 0
+            while head:
+                length += 1
+                head = head.next
+            return length
+        
+        dummy = ListNode(0, head)
+        length = getLength(head)
+        cur = dummy
+        for i in range(1, length - n + 1):
+            cur = cur.next
+        cur.next = cur.next.next
+        return dummy.next
+
+url：https://leetcode.cn/problems/remove-nth-node-from-end-of-list/solution/shan-chu-lian-biao-de-dao-shu-di-nge-jie-dian-b-61/
